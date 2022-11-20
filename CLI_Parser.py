f79914f42986b2726df14322bc87361366c961b1
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-20 14:58:30 +0530

    Added basic CLI tooling

---
+++ CLI_Parser.py
@@ -0,0 +1,16 @@
+import argparse
+
+class CLI :
+    def __init__(self) :
+        self.parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+            description="Parameters for the sudoku solver", 
+            allow_abbrev=True, 
+            add_help=True
+        )
+        
+        self.parser.add_argument("-i", help="Image absolute path", default="./Testing_Images/1.jpg")
+        # self.parser.add_argument("-w", help="Real time (Webcam)", action="store_false")
+    
+    def get_args(self) :
+        return self.parser.parse_args()
